21939af659e3cc5814cddf3ecb854523fe5627ce
kenfang <kenfangch@yahoo.com.tw>
2022-01-06 23:39:52 +0800

    Can Create Multiple Order Receipts.

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const AWS = require('aws-sdk');
 
 let s3 = new AWS.S3({
-  accessKeyId: 'YOUR_ACCESS_KEY',
+  accessKeyId: 'YOUR_ACCESS_KEY_ID',
   secretAccessKey: 'YOUR_SECRET_ACCESS_KEY'
 });
 
-exports.handler = async(event, context, callback) => {
-    let s3Bucket = 'YOUR_BUCKET_NAME';
-    let objectName = 'orderreceipt.json';
-    let objectData = JSON.stringify(event.Records[0].Sns.Message);
+async function createOrderReceipt(message_id, message) {
+    let s3Bucket = 'YOUR_BUCKET';
+    let objectKey = `${message_id}.json`;
+    let objectData = JSON.stringify(message);
     let objectType = 'application/json';
 
     try {
         let params = {
             Bucket: s3Bucket,
-            Key: objectName,
+            Key: objectKey,
             Body: objectData,
             ContentType: objectType
         };
         await s3.putObject(params).promise();
-        callback(null, `File uploaded successfully at https:/` + s3Bucket + `.s3.amazonaws.com/` + objectName);
-    } catch (err) {
-        console.error(err);
+        console.log(`File uploaded successfully at https:/` + s3Bucket + `.s3.amazonaws.com/` + objectKey );
+    } catch (error) {
+        console.error(JSON.stringify(error));
+    }
+}
+
+exports.handler = async(event, context, callback) => {
+    for (let i = 0; i <  event.Records.length; i++) {
+        let sns_record = event.Records[i].Sns;
+        await createOrderReceipt(sns_record.MessageId, sns_record.Message);
     }
 };
